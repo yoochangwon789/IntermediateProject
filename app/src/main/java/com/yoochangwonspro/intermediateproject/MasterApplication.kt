0b3083d3aa02f3a6ccfab5d754182b030e03ba12
CN <ckddnjs554@naver.com>
2021-08-09 17:31:10 +0900

    Feat : LifeCycle foreground, background 체크 하기 위한 MasterApplication 클래스 추가

--- app/src/main/java/com/yoochangwonspro/intermediateproject/MasterApplication.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/MasterApplication.kt
@@ -1,11 +1,50 @@
 package com.yoochangwonspro.intermediateproject
 
 import android.app.Application
+import android.arch.lifecycle.Lifecycle
+import android.arch.lifecycle.LifecycleObserver
+import android.arch.lifecycle.OnLifecycleEvent
+import android.arch.lifecycle.ProcessLifecycleOwner
 
-class MasterApplication : Application() {
+class MasterApplication : Application(), LifecycleObserver {
 
     override fun onCreate() {
         super.onCreate()
+        ProcessLifecycleOwner.get().lifecycle.addObserver(this)
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun onAppBackgrounded() {
+        println("check app ON_STOP")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun onAppForegrounded() {
+        println("check app ON_START")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun onAppCreated() {
+        println("check app ON_CREATE")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
+    fun onAppResumed() {
+        println("check app ON_RESUME")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
+    fun onAppPause() {
+        println("check app ON_PAUSE")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun onAppDestroyed() {
+        println("check app ON_DESTROY")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    fun onAppAny() {
+        println("check app ON_ANY")
+    }
 }
