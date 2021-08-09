0e8c8acd433f57a19e0769cb07a97351e24d65d9
CN <ckddnjs554@naver.com>
2021-08-09 19:56:54 +0900

    Feat : MyFirebaseMessagingService 클래스 생성 및 onNewToken 메서드 오버라이딩 작업

---
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/MyFirebaseMessagingService.kt
@@ -0,0 +1,14 @@
+package com.yoochangwonspro.intermediateproject
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    // 토큰은 굉장이 자주 변경이 될 수 있다
+    // 그렇기 때문에 현재 토큰을 가져와서 사용하는 경우는 실제 라이브 서비스에서는 무리가 있을 수 있다.
+    // 실제 라이브 서비스를 운영할때 onNewToken 이 호출 즉 새로운 토큰이 갱신될 때 마다
+    // 서버에다가 해당 토큰을 갱신해줘야하는 처리를 해줘야한다
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+    }
+}
