0649f5c7e533eb88a8f3e7692f39b98f23d07387
CN <ckddnjs554@naver.com>
2021-08-09 17:24:50 +0900

    Feat : LifeCycle foreground, background 체크 하기 위한 라이브러리 추가

---
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/MasterApplication.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.intermediateproject
+
+import android.app.Application
+
+class MasterApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
