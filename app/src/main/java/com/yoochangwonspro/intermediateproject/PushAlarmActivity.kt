e97592489ff1e40e2e5b1d60da8304e97483806d
CN <ckddnjs554@naver.com>
2021-08-09 16:35:28 +0900

    Feat : PushAlarmActivity 레이아웃 구성

---
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/PushAlarmActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.intermediateproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PushAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_push_alarm)
+    }
+}
