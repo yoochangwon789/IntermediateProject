05749f0a0fe527a02ad3e04a68208c3f12ad9dd8
CN <ckddnjs554@naver.com>
2021-08-09 19:17:33 +0900

    Feat : PushAlarmActivity initFirebase 함수 구현 token 받아오는 함수 구현

--- app/src/main/java/com/yoochangwonspro/intermediateproject/PushAlarmActivity.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/PushAlarmActivity.kt
@@ -3,6 +3,7 @@ package com.yoochangwonspro.intermediateproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class PushAlarmActivity : AppCompatActivity() {
 
@@ -17,5 +18,16 @@ class PushAlarmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_push_alarm)
+
+        initFirebase()
+    }
+
+    private fun initFirebase() {
+        // 파이어 베이스 토큰 가져오는 함수
+        FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                firebaseToken.text = task.result
+            }
+        }
     }
 }
