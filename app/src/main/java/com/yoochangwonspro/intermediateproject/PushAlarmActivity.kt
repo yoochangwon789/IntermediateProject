531f96a8e609af87e95f8e9930436cc30eb938c9
CN <ckddnjs554@naver.com>
2021-08-09 16:37:05 +0900

    Feat : PushAlarmActivity textView property 지정

--- app/src/main/java/com/yoochangwonspro/intermediateproject/PushAlarmActivity.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/PushAlarmActivity.kt
@@ -2,8 +2,18 @@ package com.yoochangwonspro.intermediateproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class PushAlarmActivity : AppCompatActivity() {
+
+    private val resultTextView: TextView by lazy {
+        findViewById(R.id.push_alarm_result_text_view)
+    }
+
+    private val firebaseToken: TextView by lazy {
+        findViewById(R.id.push_alarm_firebase_token_text_view)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_push_alarm)
