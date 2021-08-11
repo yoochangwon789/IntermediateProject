b16b61a2423bc840a994b7b6a0383c67627b6f99
CN <ckddnjs554@naver.com>
2021-08-11 21:03:58 +0900

    Feat : MyFirebaseMessagingService createNotification 파라미터 타입 nullable 하게 변환

--- app/src/main/java/com/yoochangwonspro/intermediateproject/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/MyFirebaseMessagingService.kt
@@ -40,7 +40,7 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         // NotificationCompat.Builder 를 실제로 notify 시키는 부분
         // 메세지를 보냈을 때 타이틀과 메세지를 맞춰서 알림을 보여주는 것을 확인할 수 있다.
         NotificationManagerCompat.from(this)
-            .notify(1, notificationBuilder.build())
+            .notify(1, createNotification(type, title, message))
     }
 
     private fun createNotificationChannel() {
@@ -59,8 +59,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
     private fun createNotification(
         type: NotificationType,
-        title: String,
-        message: String,
+        title: String?,
+        message: String?,
     ): Notification {
         // 알림 컨텐츠 생성
         // 알림이 울리게 되면 statusBar 나 좌측 상단에 설정한 아이콘이 뜨게된다
