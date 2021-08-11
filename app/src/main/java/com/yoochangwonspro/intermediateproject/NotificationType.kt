cf7ea0f7042243afbe27f9e0bcc24f1ca5f8c212
CN <ckddnjs554@naver.com>
2021-08-11 20:50:48 +0900

    Feat : NotificationType enum class 상황의 따른 title, id Type 설정

--- app/src/main/java/com/yoochangwonspro/intermediateproject/NotificationType.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/NotificationType.kt
@@ -1,4 +1,7 @@
 package com.yoochangwonspro.intermediateproject
 
-enum class NotificationType {
+enum class NotificationType(val title: String, val id: Int) {
+    NORMAL("일반 알림", 0),
+    EXPANDABLE("확장형 알림", 1),
+    CUSTOM("커스텀 알림", 2)
 }
