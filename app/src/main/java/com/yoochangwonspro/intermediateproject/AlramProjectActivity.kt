88118d8c4bcf7494a8b43630e5912bf095e76833
CN <ckddnjs554@naver.com>
2021-08-23 18:15:21 +0900

    Feat : AlramProjectActivity 프로젝트 set up

---
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/AlramProjectActivity.kt
@@ -0,0 +1,11 @@
+package com.yoochangwonspro.intermediateproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AlramProjectActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_alram_project)
+    }
+}
