aa8da7dae2c1a74dc7e7a63987123ba99043285a
CN <ckddnjs554@naver.com>
2021-08-23 19:57:36 +0900

    Feat : AlramDisplayModel data class 프로퍼티 get 함수 초기화 작업

--- app/src/main/java/com/yoochangwonspro/intermediateproject/AlramDisplayModel.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/AlramDisplayModel.kt
@@ -6,4 +6,16 @@ data class AlramDisplayModel(
     var onOff: Boolean
 ) {
 
+    val timeText: String
+        get() {
+            val h = "%02d".format(if (hour < 12) hour else hour - 12)
+            val m = "%02d".format(minute)
+
+            return "$h:$m"
+        }
+
+    val ampmText: String
+        get() {
+            return if (hour < 12) "AM" else "PM"
+        }
 }
