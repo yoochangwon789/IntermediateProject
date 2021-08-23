ea10c59d737698176a0046e43c6db69f77f3cc41
CN <ckddnjs554@naver.com>
2021-08-23 20:44:09 +0900

    Feat : AlramProjectActivity initChangeAlarmTimeButton TimePickerDialog 함수 초기 셋팅

--- app/src/main/java/com/yoochangwonspro/intermediateproject/AlramProjectActivity.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/AlramProjectActivity.kt
@@ -1,8 +1,10 @@
 package com.yoochangwonspro.intermediateproject
 
+import android.app.TimePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import java.util.*
 
 class AlramProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +20,7 @@ class AlramProjectActivity : AppCompatActivity() {
         // step2 뷰에 데이터를 그려주기
     }
 
-    private fun initChangeAlarmTimeButton() {
+    private fun initOnOffButton() {
         val onOffButton = findViewById<Button>(R.id.alram_on_off_button)
         onOffButton.setOnClickListener {
             // 데이터를 확인을 한다.
@@ -32,15 +34,19 @@ class AlramProjectActivity : AppCompatActivity() {
         }
     }
 
-    private fun initOnOffButton() {
+    private fun initChangeAlarmTimeButton() {
         val changeAlramButton = findViewById<Button>(R.id.alram_change_time_button)
         changeAlramButton.setOnClickListener {
-            // 현재시간을 일단 가져온다.
-            // TimePickDialog 띄워워서 시간을 설정을 하도록 하게끔 하고, 그 시간을 가져와서
+            // TimePickDialog 띄워줘서 시간을 설정을 하도록 하게끔 하고, 그 시간을 가져와서
 
-            // 데이터를 저장한다.
-            // 뷰를 업데이트 한다.
-            // 기존에 있던 알람을 삭제한다.
+            val calendar = Calendar.getInstance()
+
+            TimePickerDialog(this, { picker, hour, minute ->
+                // 데이터를 저장한다.
+                // 뷰를 업데이트 한다.
+                // 기존에 있던 알람을 삭제한다.
+            }, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), false)
+                .show()
         }
     }
 }
