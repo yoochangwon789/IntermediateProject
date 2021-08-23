6069266f2dc0c4465bf899e4b20c2bc0c2c89cb7
CN <ckddnjs554@naver.com>
2021-08-23 18:58:54 +0900

    Feat : AlramProjectActivity initButton 함수 추상화 작업

--- app/src/main/java/com/yoochangwonspro/intermediateproject/AlramProjectActivity.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/AlramProjectActivity.kt
@@ -7,5 +7,19 @@ class AlramProjectActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alram_project)
+
+        // step0 뷰를 초기화해주기
+        initOnOffButton()
+        initChangeAlarmTimeButton()
+
+        // step1 데이터 가져오기
+
+        // step2 뷰에 데이터를 그려주기
+    }
+
+    private fun initChangeAlarmTimeButton() {
+    }
+
+    private fun initOnOffButton() {
     }
 }
