4a9a428c2df7c04933e0e62baf3d63ae33bc0c0e
CN <ckddnjs554@naver.com>
2021-08-24 19:31:49 +0900

    Feat : AlarmReceiver class 의 createNotificationChannel 함수의 notificationChannel 생성 작업

--- app/src/main/java/com/yoochangwonspro/intermediateproject/AlarmReceiver.kt
+++ app/src/main/java/com/yoochangwonspro/intermediateproject/AlarmReceiver.kt
@@ -1,8 +1,12 @@
 package com.yoochangwonspro.intermediateproject
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationManagerCompat
 
 class AlarmReceiver : BroadcastReceiver() {
 
@@ -12,7 +16,15 @@ class AlarmReceiver : BroadcastReceiver() {
     }
 
     private fun createNotificationChannel(context: Context) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                "1000",
+                "기상 알람",
+                NotificationManager.IMPORTANCE_HIGH
+            )
 
+            NotificationManagerCompat.from(context).createNotificationChannel(notificationChannel)
+        }
     }
 
     private fun notifyNotification(context: Context) {
